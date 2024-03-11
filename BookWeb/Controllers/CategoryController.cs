c1e73d7833904da7722c32e139fc05a9b02fc545
Jamie <jamiemolseed@hotmail.co.uk>
2024-03-11 16:37:40 +0000

    Category model created, migrated to db. Category index page with genres displayed in a table format. Next stage is to create more genres

---
+++ BookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,27 @@
+﻿using BookWeb.Data;
+using BookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            // retrieve all categories from db 
+            List<Category> objCategoryList = _db.Categories.ToList();
+            // pass categories to view so it can be used in data
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
