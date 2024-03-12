89212adc477f6fd54b492ba63901133bf3540749
Jamie <jamiemolseed@hotmail.co.uk>
2024-03-12 14:42:18 +0000

    CRUD Implemented for categories - edit, create, delete, with notification displayed on Category index page on completion via a partial view, to be improved on at a later time.

--- BookWeb/Controllers/CategoryController.cs
+++ BookWeb/Controllers/CategoryController.cs
@@ -23,5 +23,83 @@ namespace BookWeb.Controllers
         {
             return View();
         }
+        [HttpPost] // when something is posted, this is  called
+        public IActionResult Create(Category category)
+        {
+           
+            if (category.Name != null && category.Name.ToLower() == "test")
+            {
+                ModelState.AddModelError("", "Test is an invalid valye");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(category);
+                _db.SaveChanges();
+                TempData["Success"] = "Category Created!";
+                return RedirectToAction("Index", "Category");
+            }
+            // Once saved, returns to Cat index with updated tab;e}
+
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category retrieveCategory = _db.Categories.Find(id);
+            if (retrieveCategory == null)
+            {
+                return NotFound();
+            }
+            return View(retrieveCategory);
+        }
+        [HttpPost] 
+        public IActionResult Edit(Category category)
+        {
+
+            
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(category);
+                _db.SaveChanges();
+                TempData["Success"] = "Category Updated!";
+                return RedirectToAction("Index", "Category");
+            }
+            
+
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category retrieveCategory = _db.Categories.Find(id);
+            if (retrieveCategory == null)
+            {
+                return NotFound();
+            }
+            return View(retrieveCategory);
+        }
+        [HttpPost, ActionName("Delete")] // Gives a different name since name and param identical, to differentiate
+        public IActionResult DeletePost(int? id)
+        {
+
+            Category? category = _db.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(category);
+            _db.SaveChanges();
+            TempData["Success"] = "Category Deleted!";
+            return RedirectToAction("Index", "Category");
+        }
+
     }
 }
